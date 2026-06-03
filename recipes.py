77ad108bbf76729ed97cf3b231fd0807fe81061d
Арустамов Андрей <avarustamov@edu.hse.ru>
2026-06-03 11:49:58 +0000

    Добавил класс Recipe с методами add_ingredient, добавляющим новый объект вида Ingredient в рецепт, и scale, возвращающим новый объект типа Recipe с увеличинными значениями.

--- recipes.py
+++ recipes.py
@@ -25,3 +25,34 @@ class Ingredient:
         if not isinstance(other, Ingredient):
             return NotImplemented
         return (self.name == other.name) and (self.unit == other.unit)
+    
+class Recipe:
+    def __init__(self, title : str, ingredients = None):
+        self.title = title
+        self.ingredients = ingredients if ingredients is not None else []
+
+    def add_ingredient(self, ingredient : Ingredient):
+        for now_ingredient in self.ingredients:
+            if now_ingredient == ingredient:
+                now_ingredient.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(self, ratio):
+        return isinstance(ratio, (int, float)) and ratio > 0
+
+    def scale(self, ratio):
+        if not self.is_valid_ratio(ratio):
+            raise ValueError("Коэффициент должен быть положительным")
+
+        scaled = [Ingredient(ingredient.name, ingredient.quantity * ratio, ingredient.unit) for ingredient in self.ingredients]
+        return Recipe(self.title, scaled)
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        ings = [ing.name for ing in self.ingredients]
+        return f"Блюдо:{self.title}. Ингредиенты: {', '.join(ings)}"
+    
