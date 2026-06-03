c8a6d30ea9b50af7fec68f3b30d4349ac7dfabee
Арустамов Андрей <avarustamov@edu.hse.ru>
2026-06-03 13:50:22 +0000

    Добаdbk класс DietaryRecipe для диетических категорий, является наследником класс Recipe. Реализовал масштабирование через метод scale и строкове представление с префиксом диеты.

--- recipes.py
+++ recipes.py
@@ -90,3 +90,14 @@ class ShoppingList:
         new_list._items = self._items + other._items
         return new_list
     
+class DietaryRecipe(Recipe):
+    def __init__(self, title : str, diet_type : str, ingredients=None):
+        super().__init__(title, ingredients)
+        self.diet_type = diet_type
+
+    def scale(self, ratio: float):
+        scaled = super().scale(ratio)
+        return DietaryRecipe(scaled.title, self.diet_type, scaled.ingredients)
+
+    def __str__(self):
+        return f"[{self.diet_type}] {super().__str__()}"
