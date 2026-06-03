61f230cf71270f1548c06eebec58e908e08e3d72
Арустамов Андрей <avarustamov@edu.hse.ru>
2026-06-03 13:21:15 +0000

    Добавил класс ShoppingList для хранения ингредиентов из рецептов с учёто количества порций. Реализован метод add_recipe для добавления рецепта и remove_recipe для удаления, метод get_list для вывода списка покупок, реализован процесс объединения 2 списков

--- recipes.py
+++ recipes.py
@@ -56,3 +56,37 @@ class Recipe:
         ings = [ing.name for ing in self.ingredients]
         return f"Блюдо:{self.title}. Ингредиенты: {', '.join(ings)}"
     
+class ShoppingList:
+    def __init__(self, ingredinet : Ingredient, recipe_title : str):
+        def __init__(self):
+            self._items = []
+
+    def add_recipe(self, recipe: Recipe, portions : float):
+        if portions <= 0:
+            raise ValueError ("Количество порций должно быть положительным")
+        scaled = recipe.scale(portions)
+        for ingredient in scaled.ingredients:
+            self._items.append((ingredient, recipe.title))
+
+    def remove_recipe(self, title : str):
+        self._items = [it for it in self._items if it[1] != title]
+
+    def get_list(self):
+        total = {}
+        for ingredient, recipe_title in self.items:
+            k = (ingredient.name, ingredient.unit)
+            if k in total:
+                total[k] += ingredient.quantity
+            else:
+                total[k] = ingredient.quantity
+
+        result = [Ingredient(name, quantity, unit) for (name, unit), quantity in total.items()]
+        return sorted(result, key=lambda ingredient: ingredient.name)
+
+    def __add__(self, other):
+        if not isinstance(other, ShoppingList):
+            return NotImplemented
+        new_list = ShoppingList()
+        new_list._items = self._items + other._items
+        return new_list
+    
